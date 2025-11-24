0ef1685cab8bdabea2473361eae56b35a9a7a416
Tiside <stasbazhan06@gmail.com>
2025-11-24 17:27:58 +0100

    Password Strenght

--- public/index.php
+++ public/index.php
@@ -11,6 +11,8 @@ $all = [];
 
 $password = [];
 
+$passwordStrenght = 0;
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $lenght = (int)$_POST["lenght"];
     $includeNumbers = isset($_POST["numbers"]);
@@ -40,11 +42,73 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $password[] = $all[$randomAll];
     }
 
+    $lettersCount = 0;
+    $numbersCount = 0;
+    $specialCount = 0;
     $generatedPassword = implode("", $password);
+
+
+    for ($i = 0; $i < strlen($generatedPassword); $i++) {
+        $char = $generatedPassword[$i];
+        if ($char >= "0" && $char <= "9") {
+            $numbersCount++;
+        } elseif ($char >= "a" && $char <= "z" || $char >= "A" && $char <= "Z") {
+            $lettersCount++;
+        } else {
+            $specialCount++;
+        }
+    }
+
+    if (count($password) >= 1 && count($password) <= 5) {
+        $passwordStrenght = 1;
+
+        if ($numbersCount > 0 || $specialCount > 0) {
+            $passwordStrenght = 2;
+        }
+
+        if ($numbersCount && $specialCount > 0) {
+            if ($numbersCount + $specialCount > $lettersCount) {
+                $passwordStrenght = 3;
+            }
+        }
+    } elseif (count($password) > 5 && count($password) <= 10) {
+        $passwordStrenght = 4;
+
+        if ($numbersCount > 0 || $specialCount > 0) {
+            $passwordStrenght = 5;
+        }
+
+        if ($numbersCount && $specialCount > 0) {
+            if ($numbersCount + $specialCount > $lettersCount) {
+                $passwordStrenght = 6;
+            }
+        }
+    } elseif (count($password) > 10 && count($password) <= 15) {
+        $passwordStrenght = 7;
+
+        if ($numbersCount > 0 || $specialCount > 0) {
+            $passwordStrenght = 8;
+        }
+        if ($numbersCount && $specialCount > 0) {
+            if ($numbersCount + $specialCount > $lettersCount) {
+                $passwordStrenght = 9;
+            }
+        }
+
+    } elseif (count($password) > 15) {
+
+        if ($numbersCount > 0 || $specialCount > 0) {
+            $passwordStrenght = 9;
+        }
+
+        if ($numbersCount + $specialCount > 10) {
+            $passwordStrenght = 10;
+        }
+    }
+
 
 }
 ?>
-
 <form action="" method="post">
     <label for="password"> Generate password</label>
     <input type="text" name="password" value="<?= $generatedPassword; ?>">
@@ -60,3 +124,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <br>
     <button type="submit">Generate</button>
 </form>
+<p>
+    Password strenght: <span><?= $passwordStrenght ?></span>
+</p>
